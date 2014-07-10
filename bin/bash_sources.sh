dc28d18d420828ec2dd6bb18f390ec72faf551d9
Thomas Furfaro <furfaro@cmre.nato.int>
2014-07-10 15:38:28 +0200

    Fixed name and location of file.

--- bin/bash_sources.sh
+++ bin/bash_sources.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # List the things you want sourced here. A decent way to turn on/off functions that are stored here.
 source $DIR/dir.sh
-source $DIR/rossource.sh
+source $DIR/ros-tools.sh
