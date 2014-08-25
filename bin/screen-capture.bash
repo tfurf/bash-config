4ad33dc2025946bb8f69aa77f8bd2b6a7d3675b4
Thomas Furfaro <furfaro@cmre.nato.int>
2014-08-25 11:15:14 +0200

    Changed scripts to check if function is available before sourcing. awesome doesn't source .bashrc or .bash_aliases or anything when invoking scripts, so we have to set up the environment manually.

--- bin/screen-capture.bash
+++ bin/screen-capture.bash
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+function exists {
+  #Check if function exists.
+  hash ${1} 2>/dev/null || { return 1 ; }
+  return 0
+}
+
+exists screen-capture || source $HOME/.bash/src/wm-tools.sh
+
 screen-capture
+
--- bin/screen-locker.bash
+++ bin/screen-locker.bash
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+function exists {
+  #Check if function exists.
+  hash ${1} 2>/dev/null || { return 1 ; }
+  return 0
+}
+
+exists screen-locker || source $HOME/.bash/src/wm-tools.sh
+
 screen-locker
