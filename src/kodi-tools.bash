0d67a35bb88649552583017bc2e486b02891d6d1
Thomas C. Furfaro <tfurfaro@gmail.com>
2015-03-16 19:16:45 +0100

    Update the kodi tools a bit.

--- src/kodi-tools.bash
+++ src/kodi-tools.bash
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 function kodi-scan-video {
-  HOST=$1
-  PORT=$2
+  [[ $# == 0 ]] && { HOST="rpi" ; PORT=80 ;}
+  [[ $# == 1 ]] && { HOST="$1" ; PORT=80 ;}
+  [[ $# == 2 ]] && { HOST="$1" ; PORT="$2" ;}
   curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' \
       -H 'content-type: application/json;' http://${HOST}:${PORT}/jsonrpc
+  echo ""
 }
 
 function kodi-clean-video {
-  HOST=$1
-  PORT=$2
-  curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Clean", "id": "mybash"}' 
+  [[ $# == 0 ]] && { HOST="rpi" ; PORT=80 ;}
+  [[ $# == 1 ]] && { HOST="$1" ; PORT=80 ;}
+  [[ $# == 2 ]] && { HOST="$1" ; PORT="$2" ;}
+  curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Clean", "id": "mybash"}' \
   -H 'content-type: application/json;' http://${HOST}:${PORT}/jsonrpc
+  echo ""
 }
