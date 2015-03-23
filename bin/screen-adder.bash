9e79b2aef19d871261a687b149992ca1d9c65c06
Thomas Furfaro <Thomas.Furfaro@cmre.nato.int>
2015-03-23 15:38:23 +0100

    Added some tools to manipulate second monitors without the gnome stuff.

---
+++ bin/screen-adder.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function exists {
+  #Check if function exists.
+  hash ${1} 2>/dev/null || { return 1 ; }
+  return 0
+}
+
+exists screen-locker || source $HOME/.bash/src/wm-tools.bash
+
+screen-adder
--- src/wm-tools.bash
+++ src/wm-tools.bash
@@ -13,3 +13,25 @@ function screen-locker () {
   gnome-screensaver-command -l
 }
 
+function list-screens () {
+  xrandr | grep "connected" | grep -v "disconnected" | cut -d' ' -f1
+}
+
+function get-primary-screen () {
+  xrandr | sed -En 's/^([^ ]+) .*connected primary.*$/\1/p'
+}
+
+function screen-adder () {
+  XRANDR_PRIMARY=$(get-primary-screen)
+  echo "Screen options are:"
+  i=1
+  list-screens |
+  for SCREEN in $(list-screens); do
+    echo "${i}) ${SCREEN}"
+    i=$((i + 1))
+  done;
+  echo "Choose one."
+  read choice
+  SECONDARY=$( list-screens | sed -n $(echo "${choice},${choice}p") )
+  xrandr --output ${SECONDARY} --auto --right-of ${XRANDR_PRIMARY}
+}
